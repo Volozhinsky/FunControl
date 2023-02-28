6521ea5c78f630e2cbbfd60153798b5702a9b2ba
Volozhinsky <vmmtorg@gmail.com>
2023-02-28 22:28:25 +0300

    Lesson 32

--- app/src/main/java/com/volozhinsky/funcontrol/MainActivity.kt
+++ app/src/main/java/com/volozhinsky/funcontrol/MainActivity.kt
@@ -2,10 +2,15 @@ package com.volozhinsky.funcontrol
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val counterView = findViewById<CounterView>(R.id.counterView)
+        findViewById<Button>(R.id.reverseButton).setOnClickListener{
+            counterView.inverseCountUp()
+        }
     }
 }
